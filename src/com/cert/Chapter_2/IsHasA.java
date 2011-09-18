58d62449cd1c0ee49c0764884cb3b264b0e1eb49
Jose Collas <jose.collas@goatstone.com>
2011-09-17 20:11:47 -0700

    adding Chapter_2 package

---
+++ src/com/cert/Chapter_2/IsHasA.java
@@ -0,0 +1,24 @@
+package com.cert.Chapter_2;
+
+
+public class IsHasA {
+	
+	public static void main(String[] args) {
+		System.out.println("xxx");
+	}
+
+	private class Car{
+		
+	}
+	// Subaroo is a car
+	private class Subaroo extends Car{
+		
+	}
+
+	// Horse Is An Animal, Has A halter
+	private class Animal { }
+	private class Horse extends Animal {
+		private String jalter;
+	}
+
+}
